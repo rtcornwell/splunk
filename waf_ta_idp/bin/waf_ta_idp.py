ee32c6a94f029ceac6ed5846e77d44f755dc3234
rtcornwell <rtcornwell@outlook.com>
2019-09-24 21:38:12 +0200

    updated

--- waf_ta_idp/bin/waf_ta_idp.py
+++ waf_ta_idp/bin/waf_ta_idp.py
@@ -7,7 +7,6 @@ import time
 import datetime
 import urllib
 import os
-from com.obs.client.obs_client import ObsClient
 import gzip
 from cStringIO import StringIO
 import urlparse
@@ -40,21 +39,13 @@ SCHEME = """<scheme>
                 <title>Instance Name</title>
                 <description>Provide a unique name for this Instance/Stanza. IE: Cloudtrace, VPCFlow</description>
             </arg>
-            <arg name="obsendpoint">
-                <title>OBSEndpoint</title>
-                <description>Endpoint of the Object Storage Service: IE: obs.eu-de.otc.t-systems.com .</description>
-            </arg>
-             <arg name="bucketfolder">
-                <title>Bucket Folder</title>
-                <description>Enter the root Bucket Folder Name where the logs are stored in the Bucket: CloudTraces, LogTanks</description>
-            </arg>
-            <arg name="bucketname">
-                <title>Bucketname</title>
-                <description>Enter the name of the Bucket on OBS soring the log traces.</description>
-            </arg>
             <arg name="idpname">
                 <title>IDPNAme</title>
                 <description>Enter the name of the IDP Definition used for authentication.</description>
+            </arg>
+            <arg name="projectid">
+                <title>ProjectID</title>
+                <description>Enter the ProjectID where the WAF server is running.</description>
             </arg>
             <arg name="username">
                 <title>User Name</title>
@@ -64,14 +55,7 @@ SCHEME = """<scheme>
                 <title>User password</title>
                 <description>Azure AD or Other user accounts password</description>
             </arg>
-            <arg name="logprefix">
-                <title>Log file name prefix</title>
-                <description>This should be the first letters of the log name: Case sensative.</description>
-            </arg>
-            <arg name="maxkeys">
-                <title>Maxkeys</title>
-                <description>Maximum numbwer of objects (files) to pull in one cycle (100-1000).</description>
-            </arg>
+            
       </args>
     </endpoint>
 </scheme>
@@ -119,23 +103,6 @@ def get_token(UserName, UserPass, IdpName):
         raise Exception('Error: ' + str(err))
     return TokenID.strip()
 
-# Function to request temporary AK/SK and security key https://docs.otc.t-systems.com/en-us/api/iam/en-us_topic_0097949518.html
-def get_ak(TokenID,Proxies=None, Verify=False):
-    url = "https://iam.eu-de.otc.t-systems.com/v3.0/OS-CREDENTIAL/securitytokens"
-    body = '{\"auth\":{\"identity\":{\"methods\": [\"token\"],\"token\":{\"id\": \"'+ TokenID.strip() +'\",\"duration-seconds\": \"900\"}}}}'
-    Header = dict()
-    Header.setdefault("Content-type", "application/json;charset=utf8")
-    resp = requests.request('post', url, data=body, headers=Header, verify=Verify, cert=None, proxies=Proxies, cookies=None, allow_redirects=False, timeout=600)
-    if resp.status_code!= 201:
-        logging.debug("get_ak: Error Retrievinbg Temp AK/SK: , errorCode:%s" % resp.status_code)
-        sys.exit(2)
-    else: 
-        data = json.loads(resp.text)
-        ak = data["credential"]["access"]
-        sk = data["credential"]["secret"]
-        st = data["credential"]["securitytoken"]
-    return (ak, sk, st)
-
 def init_stream():
     sys.stdout.write("<stream>")
 
@@ -154,57 +121,6 @@ def send_done_key(source):
     sys.stdout.write(xml.sax.saxutils.escape(source))
     sys.stdout.write("</source><done/></event>\n")
 
-# get the data from the data flow
-def read_gz_file(buffer):
-    buf = StringIO(buffer)
-    f = gzip.GzipFile(mode='rb', fileobj=buf)
-    return f.read()
-
-# Connect to OBS and Query all the objects in the Bucket to be processed. 
-# Prefix and marker limit the search. Marker is a checkpoint of last run.
-def processlogs(obsClient, BucketName, Bucket_folder, prefix=None, marker=None, max_keys=None, source=None):
-
-    resp_list = obsClient.listObjects(BucketName, marker=marker, max_keys=max_keys, prefix=Bucket_folder)
-    if resp_list.status < 300:
-        if resp_list.body.contents:
-            for content in resp_list.body.contents:
-                objectList = content.key.split('/')
-                objectKey = objectList[len(objectList)-1]
-                #Only process Logs that match the prefix passed and is a log file.
-                if content.key.endswith('.gz') and objectKey.startswith(prefix):
-                    resp_get = obsClient.getObject(BucketName,content.key, loadStreamInMemory=True)
-                    if resp_get.status < 300 :
-                        init_stream()
-                        StreamData = read_gz_file(resp_get.body.buffer)
-                        Events = json.loads(StreamData)
-                        # Parse individual events from json Data (trace events) and send event to splunk indexer.
-                        # https://docs.otc.t-systems.com/en-us/usermanual/cts/en-us_topic_0030598500.html
-                        # Each log may contain one or many events so we want to parse each event and process each individually.
-                        for Event in Events:
-                            # Test if JSON Object is returned (List) or a Json String. Convert string to list
-                            if isinstance(Event, unicode):
-                                Event_dict = json.loads(Event)
-                            else:
-                                Event_dict = Event
-                            # Convert timestamp in event from millisecond posix to seconds possix
-                            Event_dict['time'] = int(round(Event_dict['time'] / 1000))
-                            if 'record_time' in Event_dict: Event_dict['record_time'] = int(round(Event_dict['record_time'] / 1000))
-                            #Send formatted event data to sysout (Splunk Indexer)
-                            send_event(json.dumps(Event_dict), Event_dict['time'], source) 
-                        fini_stream()
-                    else:
-                        logging.debug("ProcessLogs: Error Accessing file: "+content.key+" Errocode: %s" , resp_get.returncode)
-                        sys.exit(2)
-                else:
-                    logging.info("ProcessLogs: Skipping File that is not a matching Log File: %s" , content.key)
-                 
-            return content.key, resp_list.body.next_marker
-        else:
-            return marker, None
-    else:
-        logging.debug("Processlogs: Error Accessing OBS Bucket " + BucketName + ",errorCode:%s", resp_list.status)
-        logging.debug('ProcessLogs: Error Message:%s', resp_list.errorMessage)
-        return None, None
 # prints XML error data to be consumed by Splunk
 def print_error(s):
     print "<error><message>%s</message></error>" % xml.sax.saxutils.escape(s)
@@ -254,14 +170,10 @@ def get_config(): # read XML configuration passed from splunkd
 
         # just some validation: make sure these keys are present (required)
         validate_conf(config, "name")
-        validate_conf(config, "obsendpoint")
-        validate_conf(config, "bucketfolder")
-        validate_conf(config, "bucketname")
         validate_conf(config, "idpname")
+        validate_conf(config, "projectid")
         validate_conf(config, "username")
         validate_conf(config, "userpass")
-        validate_conf(config, "logprefix")
-        validate_conf(config, "maxkeys")
         validate_conf(config, "checkpoint_dir")
     except Exception, e:
         raise Exception, "Error getting Splunk configuration via STDIN: %s" % str(e)
@@ -320,64 +232,51 @@ def run():
     ProxyPassword = None
     Proxies = None
     VerifyCert=False
-    LastMarker = None
-    FinalMarker = None
     # Read Parameters passed by Splunk Configuration
-    config = get_config()
-    Instance = config["name"]
-    OBSEndpoint = config["obsendpoint"]
-    BucketFolder = config["bucketfolder"]
-    BucketName = config["bucketname"]
-    IdpName = config["idpname"]
-    UserName = config["username"]
-    UserPass = config["userpass"]
-    MaxKeys = config["maxkeys"]
-    Prefix = config["logprefix"]
-    CheckPoint_dir = config["checkpoint_dir"]
+    # config = get_config()
+    # Instance = config["name"]
+    # IdpName = config["idpname"]
+    # ProjectID = config["projectid"]
+    # UserName = config["username"]
+    # UserPass = config["userpass"]
+    # CheckPoint_dir = config["checkpoint_dir"]
+    Instance = "waf_at_idp//WafLogs"
+    IdpName = "IDP"
+    ProjectID = "bf74229f30c0421fae270386a43315ee"
+    UserName = "robert"
+    UserPass = "pass"
+    CheckPoint_dir = "C:/temp"
     
               
     # # Setup Checkpoint file name based on Instance name. We ae parsing the name passed by Splunk
     slist = Instance.split("//")
-    InstanceName = slist[0]
+    InstanceName = slist[1]
     CheckPoint = os.path.join(CheckPoint_dir, InstanceName +".checkpoint")
     
 
     # Authenticate with IdP Initiated Federation and return Token (Powershell Script)
-    TokenID = get_token(UserName, UserPass,IdpName)
+    TokenID = get_token(UserName, UserPass, IdpName)
 
-    # Get Temporary AK/SK from IAM for Federated User
-    AK, SK, TokenID = get_ak(TokenID,Proxies,VerifyCert)
-
-    # Constructs a obs client instance with your account for accessing OBS
-    # https://docs.otc.t-systems.com/en-us/sdk_python_api/obs/en-us_topic_0080493206.html
-    obsClient = ObsClient(
-        access_key_id=AK, 
-        secret_access_key=SK, 
-        security_token=TokenID,
-        server=OBSEndpoint, 
-        proxy_host=ProxyHost, 
-        proxy_port=ProxyPort,
-        proxy_username=ProxyUserName,
-        proxy_password=ProxyPassword
-    )
-  
     # We check if the last run saved the checkpoint object so that we don't process already processed logs.
     if os.path.exists(CheckPoint):
         if os.path.getsize(CheckPoint):
             fo = open(CheckPoint, "r+" )
             LastMarker = fo.readline()
             fo.close()
-
-    while True:
-        # Start Processing Logs using the listobjects function defined above. This may cycle multiple times of more than maxkey returned.
-        FinalMarker, FinalMarkerTag = processlogs(obsClient, BucketName, BucketFolder, prefix=Prefix, marker=LastMarker, max_keys=MaxKeys, source=InstanceName)
-        if FinalMarkerTag is None:
-            fo = open(CheckPoint, "w")
-            fo.write(FinalMarker)
-            fo.close()
-            obsClient.close()
-            break
-        LastMarker = FinalMarker
+    parms = "from=1548172800000&to=1548431999000"
+    url = "https://waf.eu-de.otc.t-systems.com/v1/" + ProjectID +"/waf/event?"+ parms
+    print url
+    print TokenID
+    Header = dict()
+    body = ""
+    Header.setdefault("X-Auth-Token",TokenID.strip())
+    Header.setdefault("Content-type", "application/json;charset=utf8")
+    resp = http_get(url, Header, verify=True, cert=None, proxies=None, cookies=None)
+    if resp.status_code!= 200:
+        logging.debug("get_ak: Error Retrievinbg Events: , errorCode:%s" % resp.status_code)
+    else: 
+        data = json.loads(resp.text)
+        print data
         
 if __name__ == '__main__':
     if len(sys.argv) > 1:
